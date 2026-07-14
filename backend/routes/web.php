24f189fa645de46302585e8c058f1d5cfd876c42
Bui Cong Phuc <dinoantigravity@gmail.com>
2026-07-14 08:04:56 +0700

    test service mailplit docker cau hinh env va add route wep.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+
 
 Route::get('/', function () {
     return response()->json(['message' => 'EcomFashion API Backend is running.']);
+});
+
+Route::get('/test-mail', function () {
+    Mail::raw('Mã OTP của bạn là: 123456', function ($message) {
+        $message->to('test-user@example.com')
+                ->subject('Test Mail OTP từ Laravel Backend');
+    });
+
+    return 'Email đã được gửi! Hãy kiểm tra Mailpit ở http://localhost:8025';
 });
